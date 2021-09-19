b85cd80962881b16fe192b303584d524194d963f
Beva-Lwash <seye.ama@courrier.uqam.ca>
2021-09-19 12:34:55 -0400

    Changement à float et calcul.py

---
+++ calcul.py
@@ -0,0 +1,3 @@
+print("Abbonements annuel: 4.99:",4.99*12)
+print("Abonnements annuel avec rabais 30%:",4.99*12-(4.99*30/100))
+print("Abonnements trimestriel avec rabais 15%:",4.99*3-(4.99*15/100))
---
+++ ex3.py
@@ -0,0 +1,22 @@
+print("I will now count my chickens:")
+
+print ("Hens", 25.0+30.0/6.0)
+print("Roosters",100.0-25.0*3.0%4.0)
+
+print("Now i will count the eggs:")
+#Will show the results
+print(3.0+2.0+1.0-5.0+4.0%2.0-1.0/4.0+6.0)
+
+print("Is is true that 3.0+2.0<5.0-7.0?")
+#Compare and determine if its false
+print(3.0+2.0<5.0-7.0)
+
+print("What is 3.0+2.0?",3.0+2.0)
+print("What is 5.0-7.0?",5.0-7.0)
+
+print("Oh, that's why it's False.")
+print("How about some more.")
+#Uses other comparison
+print("Is it greater?",5.0>-2.0)
+print("Is it greater or equal?", 5.0>=-2.0)
+print("Is it less or equal?",5.0<=-2.0)
