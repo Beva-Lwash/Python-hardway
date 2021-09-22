039773e524f01fc8685f1b3e1989068ce2fca710
Beva-Lwash <seye.ama@courrier.uqam.ca>
2021-09-22 17:31:18 -0400

    Loops, functions and choices

---
+++ ex34.py
@@ -0,0 +1,15 @@
+print("""
+1. bear
+2. peacock
+3. bear
+4. peacock
+5. kangaroo
+6. python3.6
+7. platypus
+8. kangaroo
+""")
+
+animals =['bear', 'python3.6','peacock','kangaroo','whale','platypus']
+
+print(animals[1], animals[2], animals[0],animals[3],animals[4])
+print(animals[2], animals[5], animals[4])
