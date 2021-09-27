971ca6a9bab336de5a5bd1a2163271b864ea91c1
seye.ahmadou <seye.ahmadou@courrier.uqam.ca>
2021-09-26 23:01:13 -0400

    Test Flask/

--- Projects/gothonweb/app.py
+++ Projects/gothonweb/app.py
@@ -12,9 +12,9 @@ def index():
         name = request.form['name']
         greet = request.form['greet']
         greeting = f"{greet}, {name}"
-        return render_template("index.html", greeting=greeting)
+        return render_template("index_form_laid_out.html", greeting=greeting)
     else:
-        return render_template("hello_form.html")
+        return render_template("hello_form_laid_out.html")
 
 
 
